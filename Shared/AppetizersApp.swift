baecddaf33fbaf65244439f6cb10251455c8b6ec
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-02-20 14:12:21 +0000

    Initial Commit

---
+++ Shared/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Shared
+//
+//  Created by Jakub Gawecki on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
